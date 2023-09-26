a1c500d407357ef2633db2dc53adbe648690522c
gulam yazdani <gulamyazdani03@gmail.com>
2023-09-26 10:07:29 +0530

    Adding toggle to enhance functionality

--- textutilities/src/App.js
+++ textutilities/src/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 // import name1, {name2, name3, name4, name5, name6} from "./module2.mjs";
 import Navbar from './components/Navbar';
@@ -8,12 +9,13 @@ import TextForm from './components/TextForm';
 
 
 function App() {
+  const [mode, setMode] = useState('dark'); // Whether dark mode is enabled or not
   // console.log(name1);
   // console.log(name2);
   return (
     <>
     {/* <HandleCapitalize /> */}
-    <Navbar title="Text Utilities"/>
+    <Navbar title="Text Utilities" mode={mode} />
     <div className="container my-3">
       <TextForm heading="Enter the text to analyze below"/>
       {/* <About heading="About us" /> */}
--- textutilities/src/components/Navbar.js
+++ textutilities/src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,6 +34,10 @@ export default function Navbar(props) {
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-primary" type="submit">Search</button>
           </form> */}
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+            <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+          </div>
         </div>
       </div>
     </nav>
